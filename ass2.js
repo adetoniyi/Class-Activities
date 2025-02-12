85e22961a9325ea79a447b867a66141aa0de3125
adetoniyi <adetoniyi04@gmail.com>
2025-02-12 15:28:54 +0100

    add

--- ass2.js
+++ ass2.js
@@ -2,3 +2,19 @@
 
 //Largest Number in an arry 
 
+
+function secondLargest(arr) {
+    if (arr.length < 2) return null 
+    let first = -Infinity, second = -Infinity
+    for (let num of arr) {
+        if (num > first) {
+            second = first
+            first = num
+        } else if (num > second && num < first) {
+            second = num
+        }
+    }
+    return second === -Infinity ? null : second 
+}
+const arr = [5, 10, 40, 25, 57]
+console.log(secondLargest(arr))
